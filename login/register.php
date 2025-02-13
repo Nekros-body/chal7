f3fd246770a20f942830ccccbaba53aeea05e290
Nekros-body <522797@vistacollege.nl>
2025-02-13 11:57:40 +0100

    Update register.php

--- login/register.php
+++ login/register.php
@@ -2,5 +2,33 @@
 include('db.php');
 
 if(isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
     
+    if ($conn->query($sql) === TRUE) {
+        echo "new record created successfully!";
+    } else {
+        echo "kanker op man. is kanker hard fout gegaan" . $sql . "<br>" . $conn->error;
+    }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" actions="">
+        <input type="text" name="username" placeholder="Username" required><br>
+        <input type="email" name="email" placeholder="Email" required><br>
+        <input type="password" name="password" placeholder="Password" required><br>
+        <button type="submit" name="submit">Register</button>
+    </form>
+</body>
+</html>
