33ba2785b223542d298da1778a16ea3d86bcdb75
Richard3175 <525205@vistacollege.nl>
2025-02-27 11:25:31 +0100

    njn

--- Over ons, Contact & diensten/OverOns.php
+++ Over ons, Contact & diensten/OverOns.php
@@ -37,7 +37,7 @@ if (!isset($_SESSION['user_id'])) {
             position: fixed;
             top: 0;
             left: -260px;
-            width: 400px;
+            width: 250px;
             height: 100vh;
             background: #333;
             color: black;
